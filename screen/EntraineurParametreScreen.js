1ac53d861330de39bb10be58a3cb2ecb013a9819
Massi <dino53yx@gmail.com>
2022-04-22 14:20:40 +0200

    Création des screen entraineurs

---
+++ screen/EntraineurParametreScreen.js
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import { StyleSheet, Text, View, SafeAreaView, Button, Alert, ImageBackground, Image} from 'react-native';
+
+const EntraineurParametreScreen = (props) => {
+  return(
+  <SafeAreaView style={styles.container}>
+    <View>
+        <Text>Joueur Parametre Screen</Text>
+    </View>
+  </SafeAreaView>
+  );
+};
+
+//Fonction styles contenur le design en CSS
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems:'center',
+    backgroundColor:'lightgrey'
+  }
+})
+
+export default EntraineurParametreScreen;
